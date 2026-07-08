7fb14233846fccad7f163b70007bcda250e4b347
braiashow <braiashow@gmail.com>
2026-07-08 16:43:16 -0300

    Finish API

--- src/index.js
+++ src/index.js
@@ -13,13 +13,13 @@ const porta = process.env.PORTA;
 
 app.get("/", (req, res) => {
   try {
-  res.status(200).json({ mensagem: "Bem-vindo à API de Tarefas!", status: "OK" , date: new Date().toLocaleString("pt-BR", {timeZone: "America/Recife"})});
-} catch (error) {
-  res.status(500).json({ mensagem: "Erro ao iniciar a API." });
-}});
-
-app.listen(porta, () => {
-  console.log(`Servidor rodando na porta ${porta}`);
-});
-// registrando rotas do módulo de tarefas
-app.use("/tarefas", router);
+    res.status(200).json({ mensagem: "Bem-vindo à API de Tarefas!", status: "OK" , date: new Date().toLocaleString("pt-BR", {timeZone: "America/Recife"})});
+  } catch (error) {
+    res.status(500).json({ mensagem: "Erro ao iniciar a API." });
+  }});
+  
+  app.listen(porta, () => {
+    console.log(`Servidor rodando na porta ${porta}`);
+  });
+  // registrando rotas do módulo de tarefas
+  app.use("/teste", router);
